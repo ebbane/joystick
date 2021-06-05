f5ae99f6c0cde2d64ee0cb35f87c0122b412a137
ebbane <ebbanediet@gmailcom>
2021-06-05 18:39:37 +0200

    add

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, url_for, render_template, request
 from flask_socketio import SocketIO
 import socket
 import json as jsonlib
-UDP_IP = "127.0.0.1"
+UDP_IP = "127.0.0.0"
 UDP_PORT = 5005
 
 
